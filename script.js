9a589dc235e1b44ae4dd2fb0e3c463513287c32b
sakameow.github.io <getzynokos@gmail.com>
2025-02-15 07:31:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const products = [
-    { name: "Surfshark Vpn", price: "Rp10.000", image: "images/surfshark.jpg", description: "7 Day Premium, Private" },
-    { name: "Express Vpn", price: "Rp10.000", image: "images/express.jpg", description: "7 Day Premium, Private" },
-    { name: "Panda Vpn", price: "Rp10.000", image: "images/panda.jpg", description: "7 Day Premium, Private" },
-    { name: "Pia Vpn", price: "Rp10.000", image: "images/pia.jpg", description: "7 Day Premium, Private" },
-    { name: "Nord Vpn", price: "Rp10.000", image: "images/nord.jpg", description: "7 Day Premium, Private" },
-    { name: "Capcut Pro", price: "Rp25.000", image: "images/capcut.jpg", description: "Capcut Pro 1 Bulan" },
-    { name: "Alight Motion Premium", price: "Rp8.000-Rp20.000", image: "images/am.jpg", description: "1 Tahun Premium Sharing-Private" },
-    { name: "Netflix Premium", price: "Rp31.000-Rp40.000", image: "images/netflix.jpg", description: "Netflix Premium 1 Bulan 1P2U / 1 Bulan 1P1U HD4K" },
-    { name: "Suntik Like Ig & Tiktok", price: "Rp2.000-Rp16.000", image: "images/suntik.jpg", description: "Suntik Like Tiktok & Ig Fast Proses" }
+    { name: "Surfshark Vpn", price: "Rp10.000", image: "surfshark.jpg", description: "7 Day Premium, Private" },
+    { name: "Express Vpn", price: "Rp10.000", image: "express.jpg", description: "7 Day Premium, Private" },
+    { name: "Panda Vpn", price: "Rp10.000", image: "panda.jpg", description: "7 Day Premium, Private" },
+    { name: "Pia Vpn", price: "Rp10.000", image: "pia.jpg", description: "7 Day Premium, Private" },
+    { name: "Nord Vpn", price: "Rp10.000", image: "nord.jpg", description: "7 Day Premium, Private" },
+    { name: "Capcut Pro", price: "Rp25.000", image: "capcut.jpg", description: "Capcut Pro 1 Bulan" },
+    { name: "Alight Motion Premium", price: "Rp8.000-Rp20.000", image: "am.jpg", description: "1 Tahun Premium Sharing-Private" },
+    { name: "Netflix Premium", price: "Rp31.000-Rp40.000", image: "netflix.jpg", description: "Netflix Premium 1 Bulan 1P2U / 1 Bulan 1P1U HD4K" },
+    { name: "Suntik Like Ig & Tiktok", price: "Rp2.000-Rp16.000", image: "suntik.jpg", description: "Suntik Like Tiktok & Ig Fast Proses" }
 ];
 
 const productList = document.querySelector(".product-list");
